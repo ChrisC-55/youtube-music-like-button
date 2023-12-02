ffa8448ea6b05bc94f578779348234beaf072c8d
ChrisC-55 <cc463797@southeast.edu>
2023-12-01 22:49:57 -0600

    fixed  bug that could result in index error

--- YoutubeMusic.py
+++ YoutubeMusic.py
@@ -4,22 +4,21 @@ import keyboard
 
 # default hotkey for toggling like button on YouTube music is shift + =.  If that changes, this will need updated.
 
-windows = gw.getAllTitles()
-
-for title in windows:
-    if 'Firefox' in title: # if Firefox is not being used can change this name.  
-        musicWindow = title
-
 def toggleLikeSong(): 
-    title = gw.getActiveWindowTitle()
+    gameWindow = gw.getActiveWindowTitle()
 
-    gw.getWindowsWithTitle(musicWindow)[0].activate() 
+    windows = gw.getAllTitles()
+    for title in windows: #used to find new title.  Window title will change with song, this refreshes to the new title each time.
+        if 'Firefox' in title: # if Firefox is not being used can change this name.  
+            musicWindow = title
+
+    gw.getWindowsWithTitle(musicWindow)[0].activate()
     # TODO add prompt on script launch to get browser name app will be running in.
     # TODO implmement tab switching so it can find the correct tab if not already on that tab.
 
     pyautogui.hotkey('shift', '=')
 
-    gw.getWindowsWithTitle(title)[0].activate()
+    gw.getWindowsWithTitle(gameWindow)[0].activate()
 
 runkey = ']' # if you want a different hotkey you can change this
 # TODO add prompt on script launch to get hotkey user wants to use.
